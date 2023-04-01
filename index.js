8d37868435629fb063c82c7245c8086d9a9c2252
trevor wells <104236134+trevor-wells@users.noreply.github.com>
2023-04-01 16:16:22 -0600

    what a doozy

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 class Formatter {
-  //add static methods here
+  static capitalize(str){
+    return str.charAt(0).toUpperCase() + str.slice(1)
+  }
+
+  static sanitize(str){
+    return str.replace(/[^a-z0-9-\s']/gim,"")
+  }
+
+  static titleize(string){
+    const words = string.split(" ")
+    let isFirst = true
+    return words.map(word => {
+        if (isFirst === true || word !== "a" && word !== "an" && word!== "but" && word !== "of" && word!== "and" && word!== "for" && word!== "at" && word!== "by" && word!== "from" && word !== "the") {
+          isFirst = false
+          return Formatter.capitalize(word)
+        }
+        return word
+      }).join(" ")
+  }
 }
